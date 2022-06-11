4c57df6a1353478645e4979342e4884026ee220f
Luka <nickolas.kanno@gmail.com>
2022-06-11 13:51:06 -0700

    11.3

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(express.urlencoded({ extended: true })); // method built into Express.js
 // parse incoming JSON data
 app.use(express.json()); // method used to recieves incoming POST data in the form of JSON and parses it into the req.body JavaScript object
 
+app.use(express.static('public'));
+
 
 
 function filterByQuery(query, animalsArray) {
@@ -117,6 +119,22 @@ app.post('/api/animals', (req, res) => {
     }
   });
 
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/animals', (req, res) => {
+      res.sendFile(path.join(__dirname, '/public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
